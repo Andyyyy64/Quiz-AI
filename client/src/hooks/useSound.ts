8cf1b43127a11f23a93690153ce0f0b4f356e40c
Andyyyy64 <takuyarossi77@gmail.com>
2024-09-18 19:27:51 +0900

    add haptics for native app vibration

--- client/src/hooks/useSound.ts
+++ client/src/hooks/useSound.ts
@@ -1,4 +1,6 @@
 import { useRef, useEffect } from "react";
+import { Haptics } from "@capacitor/haptics";
+import { Capacitor } from "@capacitor/core";
 
 type SoundType = "correct" | "incorrect" | "intaract";
 
@@ -9,8 +11,19 @@ export const useSound = (soundType: SoundType) => {
     audioRef.current = new Audio(`/sounds/${soundType}.mp3`);
   }, [soundType]);
 
+  const triggerHapticFeedback = () => {
+    if (
+      Capacitor.getPlatform() === "ios" ||
+      Capacitor.getPlatform() === "android"
+    ) {
+      Haptics.vibrate();
+    }
+  };
+
   const play = () => {
     if (audioRef.current) {
+      // if is ios or android, trigger haptic feedback(vibration)
+      triggerHapticFeedback();
       audioRef.current.currentTime = 0; // Reset to start
       audioRef.current
         .play()
